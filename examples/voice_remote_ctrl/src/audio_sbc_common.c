a16dc235c450b16aee56d3ccc760721a2b62b2bc
Nbdaner <work41524312@163.com>
2023-02-28 15:29:30 +0800

    Update audio_sbc_common.c

--- examples/voice_remote_ctrl/src/audio_sbc_common.c
+++ examples/voice_remote_ctrl/src/audio_sbc_common.c
@@ -1,1 +1,15 @@
+/**
+ *************************************************************************************
+ * @file    sbc_common.c
+ * @brief   An implementation of SBC decoder for bluetooth.
+ *
+ * @author  LIU Liang
+ * @version 
+ *
+ * Date       Author          Revision  Change Description                     
+ * ========== =============== ========= =======================================
+ * 2023-02-28 LIU Liang        v0.1      Create
+ *
+ *************************************************************************************
+ */
 #include "audio_sbc_common.h"
